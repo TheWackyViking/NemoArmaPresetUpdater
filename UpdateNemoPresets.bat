3c96cdba3f53413a63dee000fd047eb2ffc8f72e
TheWackyViking <thewackyviking@gmail.com>
2020-03-22 14:55:12 +0100

    Update UpdateNemoPresets.bat
    Updated download link to new repo name.

--- UpdateNemoPresets.bat
+++ UpdateNemoPresets.bat
@@ -2,7 +2,7 @@
 set gitbash="C:\Program Files\Git\git-bash.exe"
 set presetLoc=%~dp0
 
-git clone https://github.com/TheWackyViking/Nemo.git temp
+git clone https://github.com/TheWackyViking/NemoArmaPresetUpdater.git temp
 
 XCOPY /E /Y "temp" "%presetLoc%"
 RMDIR /S /Q "temp"
